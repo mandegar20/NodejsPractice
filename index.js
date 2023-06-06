a258b310beba2d722a8924a87265b49e4eec9884
Mohammad Bahmani <mohammadmandegar2019@gmail.com>
2023-06-06 14:53:20 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breeds/${data}/image/random`)
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+//       console.log(res.body.message);
+
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+//         console.log('Random dog saved to file');
+//       });
+//     });
+// });
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breeds/image/random`)
+    .then((res) => {
+      console.log(res.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random dog saved to file');
+      });
+    })
+    .catch((err) => console.log(err.message));
+});
